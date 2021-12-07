7034c2a013411d4eb0b0deddde470f8d80db038f
bogdanbujor99 <bujorbogdanconstantin@gmail.com>
2021-12-07 11:30:46 +0200

    calculate x to square

--- Least_Squares_Regression.py
+++ Least_Squares_Regression.py
@@ -1,4 +1,15 @@
+from Point import Point
+
 class Least_Squares_Regression:
-    def __init__(self,x,y):
-        self.__x = x
-        self.__y = y
+    def __init__(self,array_points):
+        self.__points = array_points
+        self.__x_square = []
+        self.x_to_square()
+
+    def x_to_square(self):
+        for point in self.__points:
+            self.__x_square.append(point.get_x() * point.get_x())
+            print(point.get_x() * point.get_x())
+    
+    def x_multiplied_by_y(self):
+        self.__xy= []
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from Least_Squares_Regression import Least_Squares_Regression
+from Point import Point
+
+x = 2
+y = 2
+P = Point(x,y)
+points = []
+points.append(P)
+list = Least_Squares_Regression(points)
